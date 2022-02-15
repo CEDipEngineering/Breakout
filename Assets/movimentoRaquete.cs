63182f83e04e0b6467a1fe3173e7eeda57eec431
Carlos Eduardo Dip <carlosed3@al.insper.edu.br>
2022-02-15 11:44:01 -0300

    moving paddle

---
+++ Assets/movimentoRaquete.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoRaquete : MonoBehaviour
+{
+    [Range(1,15)]
+    public float velocidade = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(inputX,0,0)*Time.deltaTime*velocidade;
+    }
+}
